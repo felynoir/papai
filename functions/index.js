f110e6a7744b06df84b6349dc800be57d0e9aa70
dragonfly161518 <por.nattapat.01@gmail.com>
2019-09-28 03:10:07 +0700

    buggggg

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
-const { dialogflow, SimpleResponse } = require('actions-on-google')
+const { dialogflow, SimpleResponse,Permission } = require('actions-on-google')
+
+const axios = require('axios')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,16 +13,35 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 const app = dialogflow()
 
-app.intent('Test', function (conv) {
+app.intent('Restaurant_near', (conv) => {
+    axios.get("")
+})
+
+const KEY_API = "AIzaSyBiWtYREWLFOGQJVHsAPYDdm9BW9TZ57C4"
+
+app.intent('Test', function (conv,params, permissionGranted) {
 
     // คำที่ user พิมพ์มาจะอยู่ใน conv.input.raw 
     // entities ที่ใส่มาจะอยู่ใน conv.parameters
-  conv.ask(new SimpleResponse({
-    speech: `Here's an example of a simple response. ` +
-      `Which type of response would you like to see next?`,
-    text: `Here's a simple response. ` +
-      `Which response would you like to see next?`,
-  }));
+    conv.data.requestedPermission = 'DEVICE_PRECISE_LOCATION';
+    conv.ask(new Permission({
+        context: 'to locate you',
+        permissions: conv.data.requestedPermission,
+    }));
+
+    if (permissionGranted) {
+        const {
+            requestedPermission
+        } = conv.data;
+        if (requestedPermission === 'DEVICE_PRECISE_LOCATION') {
+ 
+            console.log(conv.device)
+            conv.ask(new SimpleResponse(`yeh ${conv.device} `))
+        }
+    } else {
+        return conv.close('Sorry, permission denied.');
+    }
+
     //ถ้าใช้ conv.ask บทจะยังไม่ปิดตัวเองหลังตอบ ถ้าอยากให้ปืดตัวเองเปลี่ยนเป็น conv.close
 
     // SimpleResponse จะเป็นข้อความธรรมดา ถ้าอยากรู้มากกว่านี้ให้ไปเปิด https://developers.google.com/actions/assistant/responses
