293c975a1561b26fed5908031624c0d0fba79944
dragonfly161518 <por.nattapat.01@gmail.com>
2019-09-28 01:30:33 +0700

    test intent

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
+const { dialogflow, SimpleResponse } = require('actions-on-google')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+const app = dialogflow()
+
+app.intent('Test', function (conv) {
+
+    // คำที่ user พิมพ์มาจะอยู่ใน conv.input.raw 
+    // entities ที่ใส่มาจะอยู่ใน conv.parameters
+  conv.ask(new SimpleResponse({
+    speech: `Here's an example of a simple response. ` +
+      `Which type of response would you like to see next?`,
+    text: `Here's a simple response. ` +
+      `Which response would you like to see next?`,
+  }));
+    //ถ้าใช้ conv.ask บทจะยังไม่ปิดตัวเองหลังตอบ ถ้าอยากให้ปืดตัวเองเปลี่ยนเป็น conv.close
+
+    // SimpleResponse จะเป็นข้อความธรรมดา ถ้าอยากรู้มากกว่านี้ให้ไปเปิด https://developers.google.com/actions/assistant/responses
+})
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app)
