83d7b5604da45ae9965e3d91674bafa4106c2545
dragonfly161518 <por.nattapat.01@gmail.com>
2019-09-29 01:00:52 +0700

    many type achieve

--- functions/index.js
+++ functions/index.js
@@ -1,50 +1,88 @@
-const functions = require('firebase-functions');
-const { dialogflow, SimpleResponse,Permission } = require('actions-on-google')
+const functions = require("firebase-functions");
+const {
+  dialogflow,
+  BasicCard,
+  Button,
+  Image,
+  Permission
+} = require("actions-on-google");
 
-const axios = require('axios')
+const app = dialogflow();
+const axios = require("axios").default;
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  response.send("Hello from Firebase!");
 });
 
-const app = dialogflow()
+const KEY_API = "AIzaSyBiWtYREWLFOGQJVHsAPYDdm9BW9TZ57C4";
 
-app.intent('Restaurant_near', (conv) => {
-    axios.get("")
-})
+app.intent("handle_search_place", (conv, params, confirmationGranted) => {
+  return new Promise(async (resolve, reject) => {
+    const location = conv.device !== null ? conv.device.location : undefined;
+    if (confirmationGranted) {
+      if (location) {
+        try {
+          const { latitude, longitude } = location.coordinates;
+          const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?`;
+          const radius = 2 * 1000;
+          const type = conv.user.storage.typeSearch;
+          const query = `location=${latitude},${longitude}&radius=${radius}&type=${type}&key=${KEY_API}`;
+          const res = await axios.get(url + query);
 
-const KEY_API = "AIzaSyBiWtYREWLFOGQJVHsAPYDdm9BW9TZ57C4"
-
-app.intent('Test', function (conv,params, permissionGranted) {
-
-    // คำที่ user พิมพ์มาจะอยู่ใน conv.input.raw 
-    // entities ที่ใส่มาจะอยู่ใน conv.parameters
-    conv.data.requestedPermission = 'DEVICE_PRECISE_LOCATION';
-    conv.ask(new Permission({
-        context: 'to locate you',
-        permissions: conv.data.requestedPermission,
-    }));
-
-    if (permissionGranted) {
-        const {
-            requestedPermission
-        } = conv.data;
-        if (requestedPermission === 'DEVICE_PRECISE_LOCATION') {
- 
-            console.log(conv.device)
-            conv.ask(new SimpleResponse(`yeh ${conv.device} `))
+          const restaurant = res.data.results[0];
+          const ref = restaurant.photos[0].photo_reference;
+          const place_id = restaurant.place_id;
+          const mapUrl = `https://www.google.com/maps/search/?api=1&query=${restaurant.name
+            .split(" ")
+            .join("+")}&query_place_id=${place_id}`;
+          const urlImg = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${ref}&key=${KEY_API}`;
+          conv.ask("This is my recommend to you.");
+          conv.ask(
+            new BasicCard({
+              text: restaurant.vicinity,
+              title: restaurant.name,
+              buttons: new Button({
+                title: "Take me to google map",
+                url: mapUrl
+              }),
+              image: new Image({
+                url: urlImg,
+                alt: "Image alternate text"
+              }),
+              display: "CROPPED"
+            })
+          );
+          resolve();
+        } catch (err) {
+          reject(err);
         }
-    } else {
-        return conv.close('Sorry, permission denied.');
+      }
     }
+  });
+});
 
-    //ถ้าใช้ conv.ask บทจะยังไม่ปิดตัวเองหลังตอบ ถ้าอยากให้ปืดตัวเองเปลี่ยนเป็น conv.close
+app.intent("Restaurant_near", conv => {
+  conv.user.storage.typeSearch = "restaurant";
+  const options = {
+    context: "To know your location",
+    // Ask for more than one permission. User can authorize all or none.
+    permissions: ["DEVICE_PRECISE_LOCATION"]
+  };
+  return conv.ask(new Permission(options));
+});
 
-    // SimpleResponse จะเป็นข้อความธรรมดา ถ้าอยากรู้มากกว่านี้ให้ไปเปิด https://developers.google.com/actions/assistant/responses
-})
+app.intent("Spa_near", conv => {
+  conv.user.storage.typeSearch = "spa";
+  const options = {
+    context: "To know your location",
+    // Ask for more than one permission. User can authorize all or none.
+    permissions: ["DEVICE_PRECISE_LOCATION"]
+  };
+  return conv.ask(new Permission(options));
+});
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app)
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
