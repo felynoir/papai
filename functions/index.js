b51bf1205ee81d6a82f1c307964e49f71fa0bc1c
poomsc <poom.genius@gmail.com>
2019-09-29 02:06:12 +0700

    add many many

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,8 @@ const {
   BasicCard,
   Button,
   Image,
+  Suggestions,
+  LinkOutSuggestion,
   Permission
 } = require("actions-on-google");
 
@@ -27,24 +29,28 @@ app.intent("handle_search_place", (conv, params, confirmationGranted) => {
       if (location) {
         try {
           const { latitude, longitude } = location.coordinates;
-          const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?`;
+          const url_map = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?`;
           const radius = 2 * 1000;
           const type = conv.user.storage.typeSearch;
-          const query = `location=${latitude},${longitude}&radius=${radius}&type=${type}&key=${KEY_API}`;
-          const res = await axios.get(url + query);
-
-          const restaurant = res.data.results[0];
-          const ref = restaurant.photos[0].photo_reference;
-          const place_id = restaurant.place_id;
-          const mapUrl = `https://www.google.com/maps/search/?api=1&query=${restaurant.name
-            .split(" ")
-            .join("+")}&query_place_id=${place_id}`;
+          const rankby = conv.user.storage.rankSearch;
+          const query = `location=${latitude},${longitude}&radius=${radius}&type=${type}&rankby=${rankby}&key=${KEY_API}`;
+          const res = await axios.get(url_map + query);
+          
+          const name_type = res.data.results[0];
+          const ref = name_type.photos[0].photo_reference;
+          const place_id = name_type.place_id;
+          const mapUrl = `https://www.google.com/maps/search/?api=1&query=${name_type.name
+          .split(" ")
+          .join("+")}&query_place_id=${place_id}`;
+          const urlSuggest = `http://www.google.com/search?q=${name_type.name
+          .split(" ")
+          .join("+")}&btnI`;
           const urlImg = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${ref}&key=${KEY_API}`;
           conv.ask("This is my recommend to you.");
           conv.ask(
             new BasicCard({
-              text: restaurant.vicinity,
-              title: restaurant.name,
+              text: name_type.vicinity,
+              title: name_type.name,
               buttons: new Button({
                 title: "Take me to google map",
                 url: mapUrl
@@ -56,6 +62,11 @@ app.intent("handle_search_place", (conv, params, confirmationGranted) => {
               display: "CROPPED"
             })
           );
+          // conv.ask(new Suggestions(['Suggestion 2', 'Suggestion 3']));
+          conv.ask(new LinkOutSuggestion({
+            name: 'Suggestion Link',
+            url: urlSuggest,
+          }));
           resolve();
         } catch (err) {
           reject(err);
@@ -67,6 +78,18 @@ app.intent("handle_search_place", (conv, params, confirmationGranted) => {
 
 app.intent("Restaurant_near", conv => {
   conv.user.storage.typeSearch = "restaurant";
+  conv.user.storage.rankSearch = "distance"
+  const options = {
+    context: "To know your location",
+    // Ask for more than one permission. User can authorize all or none.
+    permissions: ["DEVICE_PRECISE_LOCATION"]
+  };
+  return conv.ask(new Permission(options));
+});
+
+app.intent("Restaurant_best", conv => {
+  conv.user.storage.typeSearch = "restaurant";
+  conv.user.storage.rankSearch = "prominence"
   const options = {
     context: "To know your location",
     // Ask for more than one permission. User can authorize all or none.
@@ -77,6 +100,7 @@ app.intent("Restaurant_near", conv => {
 
 app.intent("Spa_near", conv => {
   conv.user.storage.typeSearch = "spa";
+  conv.user.storage.rankSearch = "distance"
   const options = {
     context: "To know your location",
     // Ask for more than one permission. User can authorize all or none.
@@ -84,5 +108,17 @@ app.intent("Spa_near", conv => {
   };
   return conv.ask(new Permission(options));
 });
+
+app.intent("Spa_best", conv => {
+  conv.user.storage.typeSearch = "spa";
+  conv.user.storage.rankSearch = "prominence"
+  const options = {
+    context: "To know your location",
+    // Ask for more than one permission. User can authorize all or none.
+    permissions: ["DEVICE_PRECISE_LOCATION"]
+  };
+  return conv.ask(new Permission(options));
+});
+
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
